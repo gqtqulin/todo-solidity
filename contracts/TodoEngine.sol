abf21b93b6cea3327b37616cb6723749a02e2e0f
Oleg Gataulin <gqtqulin@icloud.com>
2024-07-30 09:41:13 +0300

    init commit

---
+++ contracts/TodoEngine.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+struct Task {
+    string title;
+    string description;
+    bool completed;
+    uint256 deadline;
+}
+
+contract TodoEngine is Ownable {
+    
+    constructor() Ownable(msg.sender) { }
+
+    Task[] public tasks;
+
+    function addTask(string calldata _title, string calldata _description,
+        uint256 _deadline) external onlyOwner {
+        tasks.push(Task(
+            _title, 
+            _description,
+            false,
+            _deadline
+        ));
+    }
+
+    function changeTaskTitle(string calldata _title, uint256 index) external onlyOwner {
+        tasks[index].title = _title; 
+    }
+
+    function changeTaskStatus(uint256 index) external onlyOwner {
+        tasks[index].completed = !tasks[index].completed;
+    }
+
+    function getTask(uint256 index) external view onlyOwner
+        returns (string memory, string memory, bool, uint256)
+    {
+        Task storage task = tasks[index];
+        return (
+            task.title,
+            task.description,
+            task.completed,
+            task.deadline
+        );
+    }
+}
